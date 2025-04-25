25262a5ed96a80e8cc21766d23cede8aea45f77f
Wallace Ribeiro <wallaceribeiro72@gmail.com>
2025-04-25 12:02:58 -0300

    criando arquivos de configuração e conf do webpack e babel

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import './js/input.js';
+
+import "./libs/dayjs.js"
+
+import './styles/index.css';
+import './styles/footer.css';
+import './styles/global.css';
+import './styles/header.css';
+import './styles/main.css';
+import './styles/modal.css';
